f67c1fa7779aa655001e14b3fdf5eb629bd502e4
OhMedrano <Omedrano564@gmail.com>
2015-10-15 20:59:07 -0400

    new commit

--- app/scripts/directives/fretmove.js
+++ app/scripts/directives/fretmove.js
@@ -13,7 +13,46 @@ angular.module('guitarwebApp')
       templateUrl: 'templates/fretMove.html',
       restrict: 'EA',
       link: function postLink(scope, element, attrs) {
-    
+      		$('#bottomInnerContentMain').children().hide();
+      		$('#fretSelection').fadeIn('fast');
+
+
+    		$('#bottomTools').click(function(){
+    			$('#bottomInnerContentNav').children().removeClass('highLighter');
+    			$(this).addClass('highLighter');
+    			
+
+    			$('#bottomInnerContentMain').children().hide();
+    			$('#fretSelection').fadeIn('slow');
+
+    			
+
+    		});
+    		$('#bottomText').click(function(){
+    			$('#bottomInnerContentNav').children().removeClass('highLighter');
+    			$(this).addClass('highLighter');
+
+
+    			$('#bottomInnerContentMain').children().hide();
+    			$('#explainText').fadeIn('slow').children.hide();
+    			$('#explainTextHow').fadeIn('fast');
+    		});
+
+    		$('#explainTextTool').click(function(){
+    			$('#explainTextContent').children().hide();
+    			$('#explainTextHow').fadeIn('slow');
+
+    			$('#explainTextNav').children().removeClass('highLighter');
+    			$(this).addClass('highLighter');
+    		});
+
+    		$('#explainTextTheorys').click(function(){
+    			$('#explainTextContent').children().hide();
+    			$('#explainTextTheory').fadeIn('slow');
+
+    			$('#explainTextNav').children().removeClass('highLighter');
+    			$(this).addClass('highLighter');
+    		});
       }
     };
   });
