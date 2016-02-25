801dc64e35eedf4fa0d839a2ed139bfd74f2752b
OhMedrano <Omedrano564@gmail.com>
2016-02-25 11:21:06 -0500

    new changes

--- app/scripts/directives/guitarnecks.js
+++ app/scripts/directives/guitarnecks.js
@@ -13,7 +13,15 @@ angular.module('guitarwebApp')
       restrict: 'EA',
       replace:true,
       link: function postLink(scope, element, attrs) {
-        
+       var result;
+       var collect = [];
+        for(var x=0;x<=fretVar[0].values.length;x++){
+        	result = fretVar[0].values[x] * 708; 
+        	collect.push('The percentage is '+result+' and fret # is ' + x);
+
+        };
+        console.log(collect);
+
       }
     };
   });
