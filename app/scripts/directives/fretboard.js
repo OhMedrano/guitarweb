44123d75cd9be7d539018aeb208b3dd3abf8feb4
OhMedrano <Omedrano564@gmail.com>
2016-03-19 01:07:38 -0400

    stuck

--- app/scripts/directives/fretboard.js
+++ app/scripts/directives/fretboard.js
@@ -34,7 +34,7 @@ angular.module('guitarwebApp')
       	scope.minorScale = [0,2,3,5,7,8,10];
       	scope.pickedScale = [];
       	scope.shownNotes = [];
-      	scope.currentScale = [];
+      	scope.currentScale = scope.majorScale;
       	scope.emptyScale = [];
       	scope.nameScale;
       	scope.allScales = [{"name":"Major/Ionian", "scale":[0,2,4,5,7,9,11]},
